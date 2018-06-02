b53adc7a773c69e7c4470b00da3a23e22c2500c1
vvdspelt <vvdspelt@gmail.com>
2018-06-02 23:32:44 +0200

    commit change

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ bot.registry.registerGroup('random', 'Random');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
+bot.on('message', (message) => {
+    if(message.content == 'hello'){
+        message.reply('heya (^_^)/')
+    }
+});
+
 bot.login('NDUyNTYzNzYzMDYxNzE5MDYx.DfSKYw.BWTTqddcp-GyznpsvmYyk2veidM');
