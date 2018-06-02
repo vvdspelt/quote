21dd64a6a536acab20e7b26965dfc85e7430962f
vvdspelt <vvdspelt@gmail.com>
2018-06-02 23:16:08 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const commando = require('discord.js-commando');
+const bot = new commando.Client();
+
+bot.registry.registerGroup('random', 'Random');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + "/commands");
+
+bot.login('NDUyNTYzNzYzMDYxNzE5MDYx.DfSKYw.BWTTqddcp-GyznpsvmYyk2veidM');
